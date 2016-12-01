358ee8b6c3231769ea16adf0eacf238153785e6f
Romulo Bahiense <romulo@expensify.com>
2016-12-01 14:22:21 -0200

    inline zsh theme

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -70,3 +70,19 @@ zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f
 
 setopt auto_cd
 export CDPATH=$HOME/Projects/
+
+
+# theme based on nanotech
+
+PROMPT='%F{green}%2c%F{blue} [%f '
+RPROMPT='$(git_prompt_info) %F{blue}] %F{green}%D{%L:%M} %F{yellow}%D{%p}%f'
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%F{yellow}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%f"
+ZSH_THEME_GIT_PROMPT_DIRTY=" %F{red}*%f"
+ZSH_THEME_GIT_PROMPT_CLEAN=""
+
+if [ ! -z "$SSH_TTY" ]; then
+    PROMPT="%F{cyan}$USER@$(hostname) %F{green}%2c%F{blue} [%f "
+fi
+
